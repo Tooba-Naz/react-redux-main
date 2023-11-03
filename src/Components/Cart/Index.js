ae1b06689bb68391c7a82a6382049445dea595b7
Tooba-Naz <naz.tooba34@gmail.com>
2023-11-03 04:00:27 -0700

    update cart

--- src/Components/Cart/Index.js
+++ src/Components/Cart/Index.js
@@ -60,6 +60,7 @@ const Cart = () => {
                     <div> <h1 className="flex items-center justify-center mt-5"> Product</h1></div>
                     <div> <h1 className="flex items-center justify-center mt-5"> Price</h1></div>
                     <div> <h1 className="flex items-center justify-center mt-5">  Quantity</h1></div>
+                    <div> <h1 className="flex items-center justify-center mt-5">  Sub Total</h1></div>
                 </div>
                 {items.map((data, index) => (
                     <div key={index}>
@@ -90,6 +91,7 @@ const Cart = () => {
                                 </div>
 
                             ))} </div>
+                            <div><h1 className="flex items-center justify-center mt-5"> {data.newPrice*data.quantity}</h1></div>
                         </div>
                     </div>
                 ))}
